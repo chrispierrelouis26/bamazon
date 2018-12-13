4f607ee45d76feed5f1d6b1baaba29186e48f2dd
chris pierre <lilchr@yahoo.com>
2018-12-13 09:42:25 -0800

    added update , not working but it will soon

--- amazon.js
+++ amazon.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require('inquirer');
+var cTable = require('console.table');
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -14,34 +15,41 @@ connection.connect();
 
 connection.query("SELECT * FROM products", function (err, response) {
   if (err) throw err;
-  console.log(response);
+  console.table(response);
   inquirer.prompt([{
     type: 'input',
     name: 'option',
-    message: "What type of product would you like to buy?"
-     
+    message: "What type of product would you like to buy?", 
+    
+
+  },
+  {
+    type: "input",
+    name: "quantity",
+    message: "How many units would you like to buy?"
   }
   ]).then(function (response) {
     console.log(response)
-    connection.query("SELECT * FROM products WHERE product_name =?", response.option, function (err, result) {
+    connection.query("SELECT * FROM products WHERE id =?", response.option, function (err, result) {
       if (err) throw err;
-      console.log(result);
-      // var choice = result.options;
+     
       console.log(result[0].stock_quantity);
 
-      if(result[0].stock_quantity <= 0) {
-        
+      if (result[0].stock_quantity <= 0) {
+
         console.log("insuffient funds!");
       } else {
         console.log("not working");
       }
-    
+
     })
 
+    connection.query("UPDATE products SET stock_quantity = 10 WHERE id = 11");
 
-});
+  });
 })
 
 // connection.end();
 
+//update the item with quanity given by id
 
